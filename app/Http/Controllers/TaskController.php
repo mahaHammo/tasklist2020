1b1a4f6b62fc91392ccd60f83696849d466ec335
unknown <mahahammo98@gmail.com>
2020-03-24 15:53:51 +0200

    new

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -16,4 +16,16 @@ class TaskController extends Controller
         $task = DB::table('tasks')->find($id);
         return view('tasks.show', compact('task'));
     }
+
+    public function store(Request $request){
+        // dd($request);
+        DB::table('tasks')->insert([
+            'name' => $request->name,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+         return redirect()->back();
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,5 @@
 
 Route::get('tasks', 'TaskController@index');
 Route::get('task/{id}','TaskController@show');
-
+Route::post('store','TaskController@store');
 
